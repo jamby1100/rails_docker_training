e521baca225ccaa83551aaf0d6efd89c1be156e7
Raphael Jambalos <raphael.jambalos@gmail.com>
2020-01-30 09:56:20 +0800

    Add models, migration, seeds; Modify views to show seeded data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+author = Author.create(name: "Raphael Jambalos", kind: "Programmer")
+post = Post.create(title: "Redis", body: "This is a in-memory database often used for caching.", author_id: author.id)
+post = Post.create(title: "PostgreSQL", body: "This is a transactional database used for transactions", author_id: author.id)
+post = Post.create(title: "DynamoDB", body: "This is a NoSQL database used for concurrent workloads.", author_id: author.id)
