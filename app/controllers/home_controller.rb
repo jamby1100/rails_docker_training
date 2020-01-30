941e41b6b0240ffd8944335af9ba1b41812b4221
Raphael Jambalos <raphael.jambalos@gmail.com>
2020-01-30 11:12:18 +0800

    Add staging environment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,9 @@ class HomeController < ApplicationController
 
     @posts = Post.all
   end
+
+  def increment_async
+    ::IncrementCountWorker.perform_async(params[:post_id])
+    redirect_to root_path
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module RailsDockerTraining
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+      config.autoload_paths += %W(#{config.root}/app/workers)
+
   end
 end
